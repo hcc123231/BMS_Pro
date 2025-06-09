d2e3d380f4d0da1fa73dc6598185398925536b42
hcc <2028915986@qq.com>
2025-06-09 09:58:40 +0800

    first test

---
+++ src/BmsLogin.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class BmsLogin {
+    public BmsLogin(){
+
+    }
+    private JButton btnLogin;//登录界面的登录按钮
+    private JButton btnRegister;//登录界面的注册按钮
+    private JFrame frmGlobal;//登录界面的全局框架
+    private JEditorPane edtAccount;
+    private JEditorPane edtPassword;
+    private JLabel labAccount;
+    private JLabel labPassword;
+    private BoxLayout blAccount;
+    private BoxLayout blPassword;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -7,3 +7,5 @@ public class Main {
 
     }
 }
+
+
